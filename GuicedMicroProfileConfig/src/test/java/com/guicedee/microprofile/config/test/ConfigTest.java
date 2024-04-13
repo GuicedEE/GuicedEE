ffd6bdac0f88da934bd058bb31e086c0de379e9b
GedMarc <marc.magon@gmail.com>
2024-04-13 15:33:08 +0200

    Add Vert.x, MP Config use raw injection, JTA Updates, Remove annotation and use persistence unit name.

--- GuicedMicroProfileConfig/src/test/java/com/guicedee/microprofile/config/test/ConfigTest.java
+++ GuicedMicroProfileConfig/src/test/java/com/guicedee/microprofile/config/test/ConfigTest.java
@@ -7,6 +7,20 @@ import org.eclipse.microprofile.config.inject.*;
 @Setter
 public class ConfigTest
 {
-	@ConfigProperty
+	@ConfigProperty(name = "test")
 	private String test;
+	@ConfigProperty(name = "test2")
+	private String test2;
+	@ConfigProperty(name = "booleanTest")
+	private boolean booleanTest;
+	@ConfigProperty(name = "test3")
+	private long numeric1;
+	@ConfigProperty(name = "test3")
+	private int numeric2;
+	@ConfigProperty(name = "test3")
+	private Integer numeric3;
+	@ConfigProperty(name = "test3")
+	private Double numeric4;
+	@ConfigProperty(name = "test3")
+	private float numeric5;
 }
